8dcda53ee7caff0ed307d36b6d4c6be7f93e71f5
Mateusz Front <mateusz.front@swmansion.com>
2018-04-27 15:49:37 +0200

    added and applied formatter

--- lib/membrane_loggers/console.ex
+++ lib/membrane_loggers/console.ex
@@ -4,33 +4,29 @@ defmodule Membrane.Loggers.Console do
   print messages to console.
   """
 
-
   use Membrane.Log.Logger.Base
   alias __MODULE__.Native
 
-
-
   def handle_log(level, msg, time, tags, state) do
     with :ok <- Native.log_prefix(level, "#{time}", tags),
-          :ok <- handle_elem(msg),
-          :ok <- Native.log_sufix
-    do
-       {:ok, state}
+         :ok <- handle_elem(msg),
+         :ok <- Native.log_sufix() do
+      {:ok, state}
     else
-       {:error, reason} ->
-         {:error, reason, state}
+      {:error, reason} ->
+        {:error, reason, state}
     end
   end
-
-
 
   defp handle_elem([]), do: :ok
 
   defp handle_elem(elem) when is_list(elem) do
     [head | tail] = elem
+
     case handle_elem(head) do
       :ok ->
         handle_elem(tail)
+
       other ->
         other
     end
@@ -41,15 +37,14 @@ defmodule Membrane.Loggers.Console do
   end
 
   defp handle_elem(elem) when is_number(elem) do
-    Native.log_number elem
+    Native.log_number(elem)
   end
 
   defp handle_elem(elem) when is_binary(elem) do
-    Native.log_text elem
+    Native.log_text(elem)
   end
 
   defp handle_elem(elem) do
     {:error, :console_log, not_implemented_for: elem}
   end
-
 end
--- mix.exs
+++ mix.exs
@@ -3,36 +3,35 @@ defmodule Membrane.Loggers.Mixfile do
   Application.put_env(:bundlex, :membrane_loggers, __ENV__)
 
   def project do
-    [app: :membrane_loggers,
-     compilers: [:bundlex] ++ Mix.compilers,
-     version: "0.0.1",
-     elixir: "~> 1.3",
-     elixirc_paths: elixirc_paths(Mix.env),
-     description: "Membrane Multimedia Framework (Loggers)",
-     maintainers: ["Mateusz Nowak"],
-     licenses: ["Proprietary"],
-     name: "Membrane Loggers",
-     source_url: "https://github.com/membraneframework/membrane-loggers",
-     preferred_cli_env: [espec: :test],
-     deps: deps()]
+    [
+      app: :membrane_loggers,
+      compilers: [:bundlex] ++ Mix.compilers(),
+      version: "0.0.1",
+      elixir: "~> 1.3",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      description: "Membrane Multimedia Framework (Loggers)",
+      maintainers: ["Mateusz Nowak"],
+      licenses: ["Proprietary"],
+      name: "Membrane Loggers",
+      source_url: "https://github.com/membraneframework/membrane-loggers",
+      preferred_cli_env: [espec: :test],
+      deps: deps()
+    ]
   end
-
 
   def application do
     [extra_applications: [], mod: {Membrane.Loggers, []}]
   end
 
-
   defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_),     do: ["lib",]
-
+  defp elixirc_paths(_), do: ["lib"]
 
   defp deps do
     [
       {:membrane_core, git: "git@github.com:membraneframework/membrane-core.git"},
       {:espec, "~> 1.1.2", only: :test},
       {:bundlex, git: "git@github.com:radiokit/bundlex.git"},
-      {:membrane_common_c, git: "git@github.com:membraneframework/membrane-common-c.git"},
+      {:membrane_common_c, git: "git@github.com:membraneframework/membrane-common-c.git"}
     ]
   end
 end
