38c9aab0d3515b66858e50aaaac4884372531501
Mateusz Front <mateusz.front@swmansion.com>
2019-04-16 14:03:49 +0200

    bumped version to v0.2.5

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.Loggers.Mixfile do
   use Mix.Project
 
-  @version "0.2.4"
+  @version "0.2.5"
   @github_url "https://github.com/membraneframework/membrane-loggers"
 
   def project do
